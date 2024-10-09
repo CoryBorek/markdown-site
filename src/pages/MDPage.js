9d42b1e14ce322013073c2b08440b5d79207ea0a
Cory Borek <agentdid127cb@aol.com>
2024-10-08 23:49:51 -0400

    update report page

--- src/pages/MDPage.js
+++ src/pages/MDPage.js
@@ -7,7 +7,6 @@ import { useLocation } from 'react-router-dom';
 
 
 const MDPage = () => {
-    const debug = false;
     const location = useLocation();
     const [md, setMd] = useState("### Loading...");
     let id = location.pathname;
@@ -21,7 +20,7 @@ const MDPage = () => {
     }
 
 
-    let site = debug ? "http://localhost:3000/" + process.env.REACT_APP_REPO + "/" + id + ".md" : process.env.REACT_APP_RAW_BASE
+    let site = process.env.REACT_APP_DEBUG === "true" ? "http://localhost:3000/" + process.env.REACT_APP_REPO + "/" + id + ".md" : process.env.REACT_APP_RAW_BASE
      + process.env.REACT_APP_REPO
       + "/"
        + process.env.REACT_APP_BRANCH + "/" + id  + ".md"
@@ -31,8 +30,8 @@ const MDPage = () => {
         setMd(await fetch(site, {cache: "no-store"}).then(res => {
             console.log(res);
             if (res.status >= 400) {
-                return "### 404: File Not Found!\nTry another page, if that doesn't work, go to " + 
-                process.env.REACT_APP_GIT_BASE + process.env.REACT_APP_REPO + "/.";
+                return "### 404: File Not Found!\nTry another page, if that doesn't work, go to [" + 
+                process.env.REACT_APP_GIT_BASE + process.env.REACT_APP_REPO + "/](" + process.env.REACT_APP_GIT_BASE + process.env.REACT_APP_REPO + "/).";
             }
             return res.text();
         }))
