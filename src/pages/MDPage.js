9d6ec99786ab555bcdfc9caf0acbcf04f93108f7
Cory Borek <agentdid127cb@aol.com>
2025-01-08 09:23:10 -0500

    add source file viewing (within markdown)

--- src/pages/MDPage.js
+++ src/pages/MDPage.js
@@ -6,6 +6,23 @@ const MDPage = () => {
     const [md, setMd] = useState("### Loading...");
     let id = location.pathname;
 
+    let types = [".c", ".java"];
+
+    let site = process.env.REACT_APP_DEBUG === "true" ? "http://localhost:3000/" + process.env.REACT_APP_REPO + "/" : process.env.REACT_APP_RAW_BASE + process.env.REACT_APP_REPO + "/" + process.env.REACT_APP_BRANCH + "/";
+
+    let code = false;
+    let codeType = "";
+
+    for (var i in types) {
+	console.log(i);
+	if (id.replaceAll("_", ".").endsWith(types[i])) {
+	    site += id.replaceAll("_", ".");
+	    code = true;
+	    console.log(types[i]);
+	    codeType = types[i].substring(1);
+	}
+    }
+    
     if (id === "") {
         id = "/"
     }
@@ -14,25 +31,27 @@ const MDPage = () => {
         id += "index"
     }
 
+    if (code === false) {
+    	site += id + ".md";
+    }
 
-    let site = process.env.REACT_APP_DEBUG === "true" ? "http://localhost:3000/" + process.env.REACT_APP_REPO + "/" + id + ".md" : process.env.REACT_APP_RAW_BASE
-     + process.env.REACT_APP_REPO
-      + "/"
-       + process.env.REACT_APP_BRANCH + "/" + id  + ".md"
-
-
-    async function loadData() {
-        setMd(await fetch(site, {cache: "no-store"}).then(res => {
+    async function loadData(code, codeType) {
+	let md2 = await fetch(site, {cache: "no-store"}).then(res => {
             console.log(res);
             if (res.status >= 400) {
                 return "### 404: File Not Found!\nTry another page, if that doesn't work, go to [" + 
                 process.env.REACT_APP_GIT_BASE + process.env.REACT_APP_REPO + "/](" + process.env.REACT_APP_GIT_BASE + process.env.REACT_APP_REPO + "/).";
             }
             return res.text();
-        }))
+        });
+	
+	if (code === true) {
+		md2 = "# " + id.replaceAll("_", ".") + "\n```" + codeType + md2 + "```";
+	}
+        setMd(md2);
     }
 
-    loadData();
+    loadData(code, codeType);
     return (<div>
         <MD>
             {md}
